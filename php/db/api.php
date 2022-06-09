592f014c6fc22ad5f41a8f4cc18adbb91b132219
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-06-09 17:32:22 +0200

    bonus authore

--- php/db/api.php
+++ php/db/api.php
@@ -4,4 +4,34 @@
 
     
     header('Content-type: application/json');
-    echo json_encode($discs);
+    if(!$_GET['author'] && !$_GET['genre']){
+
+        echo json_encode($discs);
+    }else if( $_GET['author'] && !$_GET['genre'] ){
+        $return =[];
+        foreach($discs as $disc){
+            if($disc['author'] === $_GET['author']){
+                $return[] = $disc;
+            }
+        }
+        echo json_encode($return);
+
+    }else if( !$_GET['author'] && $_GET['genre'] ){
+        $return =[];
+        foreach($discs as $disc){
+            if($disc['genre'] === $_GET['genre']){
+                $return[] = $disc;
+            }
+        }
+        echo json_encode($return);
+
+    }else if( $_GET['author'] && $_GET['genre'] ){
+        $return =[];
+        foreach($discs as $disc){
+            if($disc['genre'] === $_GET['genre'] && $disc['author'] === $_GET['author']){
+                $return[] = $disc;
+            }
+        }
+        echo json_encode($return);
+
+    }
--- php/js/main.js
+++ php/js/main.js
@@ -2,16 +2,71 @@ const app = new Vue({
     el : "#app",
     data: {
         apiUrl : "http://localhost/php-ajax-dischi/php/db/api.php",
-        discs: []
+        discs: [],
+        genres:["all"],
+        authors:["all"],
+        authorSel : "all",
+        genreSel : "all"
     },
     methods: {
         getApi(){
             axios.get(this.apiUrl)
             .then((result) => {
                 this.discs = result.data
-             console.log(result.data)   
+                console.log(result.data)   
+                this.discs.forEach(obj => {
+                    if(!this.genres.includes(obj.genre)){
+                        this.genres.push(obj.genre)
+                    }
+                });
+                this.discs.forEach(obj => {
+                    if(!this.authors.includes(obj.author)){
+                        this.authors.push(obj.author)
+                    }
+                });
+                console.log(this.author)
             })
+        },
+
+        getfilteredApi(){
+            if(!(this.authorSel === "all") && !(this.genreSel === "all")){
+                const parimi = {
+                    author : this.authorSel ,
+                    genre : this.genreSel ,
+                }
+                axios.get(this.apiUrl, {
+                    params : parimi
+                })
+                .then(r=>{
+                    this.discs = r.data
+                    console.log(r.data)
+                })
+            }else if(!(this.authorSel === "all") && this.genreSel === "all"){
+                const parimi = {
+                    author : this.authorSel 
+                }
+                axios.get(this.apiUrl, {
+                    params : parimi
+                })
+                .then(r=>{
+                    this.discs = r.data
+                    console.log(r.data)
+                })
+            }else if(!(this.authorSel === "all") && this.genreSel === "all"){
+                const parimi = {
+                    genre : this.genreSel 
+                }
+                axios.get(this.apiUrl, {
+                    params : parimi
+                })
+                .then(r=>{
+                    this.discs = r.data
+                    console.log(r.data)
+                })
+            }
+            
         }
+
     },
     mounted() {
         this.getApi()
